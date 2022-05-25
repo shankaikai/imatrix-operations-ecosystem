897a2ec3e95ae60ae7dbcf8f65185bce55c0eb19
jeanmah <jeanmah.828@gmail.com>
2022-05-25 14:20:06 +0800

    added last replied broadcast recipient

--- backend/database/broadcast_db_utilities.go
+++ backend/database/broadcast_db_utilities.go
@@ -8,12 +8,10 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-	"time"
 
 	"capstone.operations_ecosystem/backend/common"
 	pb "capstone.operations_ecosystem/backend/proto"
 	_ "github.com/go-sql-driver/mysql"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 const (
@@ -31,11 +29,12 @@ const (
 
 	// Broadcast recipients table fields
 	// Broadcast table fields
-	BC_REC_DB_ID         = "broadcast_recipients_id"
-	BC_REC_DB_RELATED_BC = "related_broadcast"
-	BC_REC_DB_RECIPIENT  = "recipient"
-	BC_REC_DB_ACK        = "acknowledged"
-	BC_REC_DB_REJECTION  = "rejected"
+	BC_REC_DB_ID           = "broadcast_recipients_id"
+	BC_REC_DB_RELATED_BC   = "related_broadcast"
+	BC_REC_DB_RECIPIENT    = "recipient"
+	BC_REC_DB_ACK          = "acknowledged"
+	BC_REC_DB_REJECTION    = "rejected"
+	BC_REC_DB_LAST_REPLIED = "last_replied"
 )
 
 // UTILITIES
@@ -59,14 +58,16 @@ func getBroadcastTableFields() string {
 
 // This function is highly dependent on the
 // order given in getBroadcastTableFields.
+// Returns the values of the broadcast fields in the
+// order that is specified in getBroadcastTableFields
 func orderBroadcastFields(broadcast *pb.Broadcast) string {
 	output := ""
 
 	output += "'" + getBroadcastDBTypeStringFromProto(broadcast.Type) + "'" + ", "
 	output += "'" + broadcast.Title + "'" + ", "
 	output += "'" + broadcast.Content + "'" + ", "
-	output += "'" + broadcast.CreationDate.AsTime().Format("2006-01-02 15:04:05") + "'" + ", "
-	output += "'" + broadcast.Deadline.AsTime().Format("2006-01-02 15:04:05") + "'" + ", "
+	output += "'" + broadcast.CreationDate.AsTime().Format(DATETIME_FORMAT) + "'" + ", "
+	output += "'" + broadcast.Deadline.AsTime().Format(DATETIME_FORMAT) + "'" + ", "
 	output += "'" + strconv.Itoa(int(broadcast.Creator.UserId)) + "'"
 
 	return output
@@ -76,19 +77,24 @@ func orderBroadcastFields(broadcast *pb.Broadcast) string {
 // in a specific order.
 // Note that IDs are auto incremented and should
 // not be modified manually. Ommits ID in resulting string.
+// All nullable columns that we do not expect to be used upon
+// first insert shall be omitted.
 func getBroadcastRecTableFields() string {
 	broadcastRecTableFields := []string{
 		BC_REC_DB_RELATED_BC,
 		BC_REC_DB_RECIPIENT,
 		BC_REC_DB_ACK,
 		BC_REC_DB_REJECTION,
+		// BC_REC_DB_LAST_REPLIED,
 	}
 
 	return strings.Join(broadcastRecTableFields, ",")
 }
 
 // This function is highly dependent on the
-// order given in getBroadcastTableFields.
+// order given in getBroadcastRecTableFields.
+// Returns the values of the broadcast fields in the
+// order that is specified in getBroadcastRecTableFields
 func orderBroadcastRecFields(recipeint *pb.BroadcastRecipient, relatedBCId int64) string {
 	output := ""
 
@@ -97,10 +103,12 @@ func orderBroadcastRecFields(recipeint *pb.BroadcastRecipient, relatedBCId int64
 
 	// Ack and rejection are fale by default.
 	output += "0, 0"
-
 	return output
 }
 
+// This function is used in anticipation of an update query.
+// Updates use the format "SET field1=val1, field2=val2".
+// The return value of this function is for example: "field1=val1, field2=val2"
 // ID not included in this because pk should not be manually changed.
 // Note this function highly depends on the protocol buffer message definition
 // Returns the string fields and values of the filled broadcast fields
@@ -126,9 +134,12 @@ func getFilledBroadcastFields(broadcast *pb.Broadcast) string {
 	return strings.Join(broadcastTableFields, ",")
 }
 
+// This function is used in anticipation of an update query.
+// Updates use the format "SET field1=val1, field2=val2".
+// The return value of this function is for example: "field1=val1, field2=val2"
 // ID not included in this because pk should not be manually changed.
 // Note this function highly depends on the protocol buffer message definition
-// Returns the string fields and values of the filled broadcast fields
+// Returns the string fields and values of the filled broadcast recipinets fields
 func getFilledBroadcastRecFields(bRec *pb.BroadcastRecipient) string {
 	broadcastTableFields := []string{}
 
@@ -139,10 +150,14 @@ func getFilledBroadcastRecFields(bRec *pb.BroadcastRecipient) string {
 	broadcastTableFields = append(broadcastTableFields, formatFieldEqVal(BC_REC_DB_ACK, strconv.FormatBool(bRec.Acknowledged), false))
 	broadcastTableFields = append(broadcastTableFields, formatFieldEqVal(BC_REC_DB_REJECTION, strconv.FormatBool(bRec.Rejected), false))
 
+	if bRec.LastReplied != nil {
+		broadcastTableFields = append(broadcastTableFields, formatFieldEqVal(BC_DB_DEADLINE, bRec.LastReplied.AsTime().Format(DATETIME_FORMAT), true))
+	}
+
 	return strings.Join(broadcastTableFields, ",")
 }
 
-// Returns the Broadcast Type as expected in the DB
+// Returns the Broadcast Type as expected in the DB from the protobuf enum
 func getBroadcastDBTypeStringFromProto(bcType pb.Broadcast_BroadcastType) string {
 	switch bcType {
 	case pb.Broadcast_ANNOUNCEMENT:
@@ -152,7 +167,7 @@ func getBroadcastDBTypeStringFromProto(bcType pb.Broadcast_BroadcastType) string
 	}
 }
 
-// Returns the Broadcast Type as expected in the DB
+// Returns the Broadcast Type as expected in the protobuf enum from the DB enum
 func getBroadcastProtoTypeStringFromDB(bcType string) pb.Broadcast_BroadcastType {
 	switch bcType {
 	case "Announcement":
@@ -162,6 +177,9 @@ func getBroadcastProtoTypeStringFromDB(bcType string) pb.Broadcast_BroadcastType
 	}
 }
 
+// Helper function to add a new filter to the list of existing
+// filters in a broadcast query struct.
+// Modifies the broadcast query parameter directly.
 func addBroadcastFilter(query *pb.BroadcastQuery, field pb.BroadcastFilter_Field,
 	comparison pb.Filter_Comparisons,
 	value string) {
@@ -172,6 +190,11 @@ func addBroadcastFilter(query *pb.BroadcastQuery, field pb.BroadcastFilter_Field
 	query.Filters = append(query.Filters, &pb.BroadcastFilter{Field: field, Comparisons: filter})
 }
 
+// Converts the filters in the broadcast array into a formatted where clause
+// that can be parsed into MySQL. If a limit is needed, the LIMIT filter is
+// added to the end of the string.
+// For example returns: "WHERE id=22 AND num <2 LIMIT 5"
+// Returns the formatted SQL filter string.
 func getFormattedBroadcastFilters(query *pb.BroadcastQuery, table string, needLimit bool) string {
 	output := ""
 
@@ -237,11 +260,10 @@ func getFormattedBroadcastFilters(query *pb.BroadcastQuery, table string, needLi
 	return output
 }
 
-// get the user's corresponding to the id in the db
+// Get the user corresponding to a particular user id in the db
 func idUserByUserId(db *sql.DB, userId int) (*pb.User, error) {
-	comparison := &pb.Filter{Comparison: pb.Filter_EQUAL, Value: strconv.Itoa(userId)}
-	userFilter := &pb.UserFilter{Field: pb.UserFilter_USER_ID, Comparisons: comparison}
-	userQuery := &pb.UserQuery{Limit: 1, Filters: []*pb.UserFilter{userFilter}}
+	userQuery := &pb.UserQuery{Limit: 1}
+	addUserFilter(userQuery, pb.UserFilter_USER_ID, pb.Filter_EQUAL, strconv.Itoa(userId))
 
 	users, err := GetUsers(db, userQuery)
 
@@ -254,6 +276,11 @@ func idUserByUserId(db *sql.DB, userId int) (*pb.User, error) {
 	return user, err
 }
 
+// This function converts the returned DB rows into Broadcast objects and
+// their corresponding broadcast recipients.
+// These rows come from the join query of both the broadcast and broadcast
+// recipients table.
+// Modifies the broadcast array in place.
 func convertDbRowsToBcNBcR(db *sql.DB, broadcasts *[]*pb.Broadcast, rows *sql.Rows, queryLimit int) error {
 	broadcastMap := make(map[int64]*pb.Broadcast)
 
@@ -267,6 +294,7 @@ func convertDbRowsToBcNBcR(db *sql.DB, broadcasts *[]*pb.Broadcast, rows *sql.Ro
 		deadlineStr := ""
 		recipientUserId := -1
 		relatedBroadcast := ""
+		var lastRepliedString sql.NullString
 
 		// cast each row to a broadcast
 		err := rows.Scan(
@@ -282,6 +310,7 @@ func convertDbRowsToBcNBcR(db *sql.DB, broadcasts *[]*pb.Broadcast, rows *sql.Ro
 			&recipientUserId,
 			&broadcastRecipient.Acknowledged,
 			&broadcastRecipient.Rejected,
+			&lastRepliedString,
 		)
 
 		if err != nil {
@@ -308,22 +337,19 @@ func convertDbRowsToBcNBcR(db *sql.DB, broadcasts *[]*pb.Broadcast, rows *sql.Ro
 			}
 
 			broadcast.Creator = creator
-			dateFormat := "2006-01-02 15:04:05"
 
-			creationDate, err := time.Parse(dateFormat, creationDateStr)
+			broadcast.CreationDate, err = DBDatetimeToPB(creationDateStr)
 			if err != nil {
 				fmt.Println("GetBroadcasts:", err.Error())
 				continue
 			}
 
-			deadline, err := time.Parse(dateFormat, deadlineStr)
+			broadcast.Deadline, err = DBDatetimeToPB(deadlineStr)
 			if err != nil {
 				fmt.Println("GetBroadcasts:", err.Error())
 				continue
 			}
 
-			broadcast.CreationDate = &timestamppb.Timestamp{Seconds: creationDate.Unix()}
-			broadcast.Deadline = &timestamppb.Timestamp{Seconds: deadline.Unix()}
 			broadcastMap[broadcast.BroadcastId] = broadcast
 		}
 
@@ -331,6 +357,14 @@ func convertDbRowsToBcNBcR(db *sql.DB, broadcasts *[]*pb.Broadcast, rows *sql.Ro
 		if err != nil {
 			fmt.Println("GetBroadcasts:", err.Error())
 			continue
+		}
+
+		if lastRepliedString.Valid {
+			broadcastRecipient.LastReplied, err = DBDatetimeToPB(lastRepliedString.String)
+			if err != nil {
+				fmt.Println("GetBroadcasts:", err.Error())
+				continue
+			}
 		}
 
 		// Add recipient to broadcast
